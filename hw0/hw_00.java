d1199ff32456b84d3e1dda5c3c52ec3c07b4b461
crazytaxi <michael.jin.1729@gmail.com>
2015-02-09 22:30:19 -0500

    added more work

--- hw0/hw_00.java
+++ hw0/hw_00.java
@@ -5,11 +5,14 @@
 import java.util.*;
 
 public class hw_00 {
+	
 	public static void main(String[] args) {
 		System.out.println(factorial(5));
 		System.out.println(fib(5, new ArrayList<Integer>()));
 		System.out.println(sqrtg(81, 1));
+		System.out.println(permute("fight"));
 	}
+
 	public static double factorial(int i) {
 		if (i == 0) {
 			return 1;
@@ -18,6 +21,7 @@ public class hw_00 {
 			return i * factorial(i - 1);
 		}
 	}
+
 	public static Integer fib (int n, ArrayList<Integer> l) {
 		if (l.size() == 0) {
 			l.add(new Integer(1));
@@ -31,6 +35,7 @@ public class hw_00 {
 			return fib(n,l);
 		}
 	}
+
 	public static double sqrtg (double i, double g) {
 		if ((g * g) < i + .001 && (g * g) > i - .001) {
 			return g;	
@@ -38,5 +43,46 @@ public class hw_00 {
 		else {
 			return sqrtg(i, ((i/g) + g) * .5);
 		}	
+	}
+
+	public static HashMap<String, Boolean> permute(String s, int i, HashMap<String, Boolean> strs) {
+		if (i == s.length()) {
+			return strs;
+		}
+		else {
+			boolean dup = false;
+			for (int k = 0; k < s.length() && k != i; k++) {
+				if (s.toCharArray()[k] == s.toCharArray()[i]) {
+					dup = true;
+					break;
+				}
+			}
+			if (!dup) {
+				for (int k = 0; k < s.length(); k++) {
+					if (!strs.containsKey(swap(s,i,k))) {
+						strs.put(swap(s,i,k), (new Boolean(true)));
+						s = swap(s,i,k);
+						permute(s,i,strs);
+					}
+				}
+			}
+		}
+		return permute(s,i+1,strs);
+	}
+
+	public static ArrayList<String> permute (String s) {
+		Object[] perm = permute(s, 0, (new HashMap<String, Boolean>())).keySet().toArray();
+		ArrayList<String> buff = new ArrayList<String> ();
+		for (int i = 0; i < perm.length; i++) {
+			buff.add((String)perm[i]);
+		}
+		return buff;
+	}
+
+	public static String swap (String s, int a, int b) {
+		char[] lol = s.toCharArray();
+		lol[a] = lol[b];
+		lol[b] = s.toCharArray()[a];
+		return (new String(lol));
 	}
 }
